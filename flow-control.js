1e7bac080c5cee0c66f9b92d0f3f3960c5859252
Andrew Persad <akpersad@gmail.com>
2020-02-06 02:26:31 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -33,7 +33,7 @@ function ternaryTeenager(age) {
 function switchAge(age) {
   if (age >= 13 && age <= 19) {
     return "You are a teenager";
-  } else if (!age <= 13 && !age >= 19) {
+  } else if (age <= 13 && age >= 19) {
     return "You have an age";
   }
 }
